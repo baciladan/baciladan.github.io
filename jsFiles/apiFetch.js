08fefe44ef7e3c3f397235583afe0b44fc5e30bb
baciladan <baciladan0@gmail.com>
2021-03-03 14:40:53 +0200

    Added BTC API and Small Modifications

--- jsFiles/apiFetch.js
+++ jsFiles/apiFetch.js
@@ -1,9 +1,9 @@
-const baseUrl = 'https://numbersapi.com/8/22/date';
+const baseUrl = 'https://api.coindesk.com/v1/bpi/currentprice.json';
 const responseContainer = document.getElementById('apiResponseContainer');
 
 function handleResponse(response) {
     if (response.status >= 200 && response.status <= 299) {
-        return response.text();
+        return response.json();
     } else {
         throw 'Oops, something went wrong with the API';
     }
@@ -15,11 +15,18 @@ function showError(message) {
 }
 
 function showResponse(parsedResponse) {
-    responseContainer.append(parsedResponse);
+    Object.keys(parsedResponse.bpi).forEach(key => {
+        let symbol = parsedResponse.bpi[key].symbol;
+        let price = parsedResponse.bpi[key].rate;
+        let symbolContainer = document.createElement('p');
+        symbolContainer.innerHTML = symbol;
+        let finalResponse = document.createElement('div');
+        finalResponse.append('current price in: ', symbolContainer, price);
+        responseContainer.appendChild(finalResponse);
+    })
 }
 
 function sendrequest(url) {
-    console.log('Fetch made!');
     fetch(url, { method: 'GET' })
         .then(handleResponse)
         .then(showResponse)
