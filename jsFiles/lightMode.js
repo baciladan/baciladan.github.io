836a41f48d7488a78ea82cb0d30a735e7a54dae0
baciladan <baciladan0@gmail.com>
2021-04-21 23:02:51 +0300

    CSS Fiexes

--- jsFiles/lightMode.js
+++ jsFiles/lightMode.js
@@ -6,30 +6,26 @@ const switchIcon = document.getElementsByClassName("fa-adjust")[0];
 let infoThemeContainer = document.getElementById("info-theme");
 
 const enableLightMode = () => {
-  document.body.classList.add("lightMode");
-  infoThemeContainer.innerText = "dark theme";
-  switchIcon.classList.remove("rotateLeft");
-  switchIcon.classList.add("rotateRight");
-  localStorage.setItem("lightMode", "enabled");
+    document.body.classList.add("lightMode");
+    infoThemeContainer.innerText = "dark theme";
+    localStorage.setItem("lightMode", "enabled");
 };
 
 const disableLightMode = () => {
-  document.body.classList.remove("lightMode");
-  infoThemeContainer.innerText = "light theme";
-  switchIcon.classList.remove("rotateRight");
-  switchIcon.classList.add("rotateLeft");
-  localStorage.setItem("lightMode", null);
+    document.body.classList.remove("lightMode");
+    infoThemeContainer.innerText = "light theme";
+    localStorage.setItem("lightMode", null);
 };
 
 if (lightMode === "enabled") {
-  enableLightMode();
+    enableLightMode();
 }
 
 lightModeSwitch.addEventListener("click", () => {
-  lightMode = localStorage.getItem("lightMode");
-  if (lightMode !== "enabled") {
-    enableLightMode();
-  } else {
-    disableLightMode();
-  }
+    lightMode = localStorage.getItem("lightMode");
+    if (lightMode !== "enabled") {
+        enableLightMode();
+    } else {
+        disableLightMode();
+    }
 });
