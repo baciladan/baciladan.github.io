8900d35a2162eaf7c80b4b973a14dc11f2cb150b
baciladan <baciladan0@gmail.com>
2022-04-04 19:03:42 +0300

    Update to Version 2.0 waiting page

--- jsFiles/responsiveNav.js
+++
@@ -1,8 +0,0 @@
-// Nav Bar Animation
-
-const burger = document.querySelector('.burger');
-const navList = document.querySelector('.nav-links')
-
-burger.addEventListener("click",function(){
-    navList.classList.toggle('nav-active');
-})
