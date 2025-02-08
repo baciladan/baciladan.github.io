c43cea3b9b122ffa9ffb6feb1548d40eda0fb139
baciladan <baciladan0@gmail.com>
2025-02-08 22:35:49 +0200

    Base homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 function App() {
   return (
-    <div className="h-screen w-screen bg-neutral-100 flex items-center justify-center">
-      <h1>Sup?</h1>
+    <div className="h-screen w-screen bg-neutral-900 flex items-center justify-center text-2xl text-neutral-200">
+      <p>
+        Keep in touch on
+        <a
+          className="text-orange-600"
+          href="https://github.com/baciladan"
+          target="_blank"
+        >
+          {" "}
+          Github{" "}
+        </a>
+        and
+        <a
+          className="text-orange-600"
+          href="https://linkedin.com/in/dan-bacila"
+          target="_blank"
+        >
+          {" "}
+          LinkedIn{" "}
+        </a>
+        !
+      </p>
     </div>
   );
 }
