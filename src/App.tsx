9c00eddafb153be9d22b89118719df49388c698d
baciladan <baciladan0@gmail.com>
2025-02-08 22:28:34 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen w-screen bg-neutral-100 flex items-center justify-center">
+      <h1>Sup?</h1>
+    </div>
+  );
+}
+
+export default App;
