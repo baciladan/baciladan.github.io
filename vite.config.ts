241c0bc71a6345776a7e5b27c2036d5328ac83f7
baciladan <baciladan0@gmail.com>
2025-02-09 11:28:48 +0200

    Updated deploy configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "",
   plugins: [react()],
   resolve: {
     alias: {
